819407841be731347af3ee419faa270fa7e8eaf6
Fish <conner.leuk@gmail.com>
2021-08-27 12:23:42 +0200

    added tournament field to team

--- src/schemas/Team.ts
+++ src/schemas/Team.ts
@@ -1,6 +1,7 @@
 import { Field, InputType, Int, ObjectType } from "type-graphql";
 import Player from "./Player";
 import { TeamPlayer } from "./TeamPlayer";
+import Tournament from "./Tournament";
 
 @ObjectType("Team")
 export default class Team {
@@ -10,12 +11,15 @@ export default class Team {
   @Field({ description: "The unique team name" })
   name: string;
 
-  @Field((type) => Player, {
+  @Field((_type) => Tournament, { description: "The unique team name" })
+  tournament: Tournament;
+
+  @Field((_type) => Player, {
     description: "The owner of the team, this player holds the permissions",
   })
   owner: Player;
 
-  @Field((type) => [TeamPlayer], { description: "An array of team players" })
+  @Field((_type) => [TeamPlayer], { description: "An array of team players" })
   teamPlayers: TeamPlayer[];
 
   //@Field((type) => [Tournament], { description: "An array of players" })
