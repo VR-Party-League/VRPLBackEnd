af5fb58544874c088ba36b3ffb48949adf11511e
Fish <conner.leuk@gmail.com>
2021-09-05 11:55:00 +0200

    started working on strike system

---
+++ src/db/models/vrplStrikes.ts
@@ -0,0 +1,34 @@
+import { Schema, model, Document } from "mongoose";
+
+export enum VrplStrikeType {
+  tempBan = "tempBan",
+  restrict = "restrict",
+}
+
+export interface VrplStrike {
+  id: string;
+  filedAt: Date;
+  offenderId: string;
+  punishment: VrplStrikeType;
+  duration: string;
+  violation: string;
+  notes: string;
+  filedById: string;
+}
+
+const StrikeSchema = new Schema<VrplStrike & Document>(
+  {
+    id: { type: String, required: true, unique: true },
+  },
+  { collection: "strikes" }
+);
+
+const StrikeModel = model<VrplStrike & Document>("strikes", StrikeSchema);
+export default StrikeModel;
+
+// TODO: appeals
+// TODO: punishments
+// TODO: all of this really
+/*
+  
+*/
