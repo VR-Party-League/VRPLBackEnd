dd2ac1c246b511dcd105c273107e8eecdd7521ee
Fish <conner.leuk@gmail.com>
2021-09-12 21:55:57 +0200

    fixed the lockfile

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,4 +2,7 @@ import { Router } from "express";
 import api from "./api";
 const router = Router();
 router.use("/api", api);
+router.get("/OwO", (req, res) => {
+  return res.send({ message: "OwO, what this" });
+});
 export default router;
