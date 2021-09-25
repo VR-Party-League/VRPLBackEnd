18549830e0ba0e68a28ca1ec95731b025b7b8503
Fish <conner.leuk@gmail.com>
2021-09-25 23:13:04 +0200

    added server permission

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -4,11 +4,12 @@ import { VrplPlayer } from "../db/models/vrplPlayer";
 export enum Permissions {
   None = 0,
   Admin = 1 << 0, // 0001 -- the bitshift is unnecessary, but done for consistency
-  ManageTeams = 1 << 1, // 0010
+  Server = 1 << 1,
   ManageTournaments = 1 << 2, // 0100
   ManageMatches = 1 << 3, // 1000
   ManagePlayers = 1 << 4,
   ManageBadges = 1 << 5,
+  ManageTeams = 1 << 6, // 0010
   AccessDiscordId = 1 << 10,
 
   //All = ~(~0 << 2), // 0111
