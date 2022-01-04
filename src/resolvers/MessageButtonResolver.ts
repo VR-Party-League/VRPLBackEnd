edeb3b8997596a47bf56f9c4375fc6ec7ea9d6e1
Fish <conner.leuk@gmail.com>
2022-01-04 23:51:45 +0100

    fixed performMessageButtonAction

--- src/resolvers/MessageButtonResolver.ts
+++ src/resolvers/MessageButtonResolver.ts
@@ -11,7 +11,7 @@ import {
   Root,
 } from "type-graphql";
 import { vrplMessageButton } from "../db/models/vrplMessages";
-import { Permissions } from "../utils/permissions";
+import { Permissions, userHasPermission } from "../utils/permissions";
 import {
   BadRequestError,
   InternalServerError,
@@ -19,6 +19,7 @@ import {
 } from "../utils/errors";
 import { getMessageForPlayerFromId, performButtonAction } from "../db/messages";
 import Message from "../schemas/Message";
+import { getPlayerFromId } from "../db/player";
 
 @Resolver((_of) => MessageButton)
 export default class {
@@ -31,13 +32,22 @@ export default class {
   @Authorized()
   @Mutation((_returns) => Message)
   async performMessageButtonAction(
+    @Arg("playerId") playerId: string,
     @Arg("buttonId") buttonId: string,
     @Arg("messageId") messageId: string,
     @Ctx() ctx: any
   ) {
     const user = ctx.user;
-    if (!user) throw new UnauthorizedError("No user found");
-    const message = await getMessageForPlayerFromId(user.id, messageId);
+    if (!user) throw new UnauthorizedError();
+    const player = await getPlayerFromId(playerId);
+    if (!player) throw new BadRequestError("Player not found");
+    else if (
+      player.id !== user.id &&
+      !userHasPermission(user, Permissions.ManageMessages)
+    )
+      throw new UnauthorizedError();
+
+    const message = await getMessageForPlayerFromId(playerId, messageId);
     if (!message) throw new BadRequestError("Message not found");
     const messageButton = message.buttons.find(
       (button) => button.id === buttonId
@@ -46,7 +56,7 @@ export default class {
     if (messageButton.clickedAt)
       throw new BadRequestError("Button already clicked");
 
-    const res = await performButtonAction(messageButton, messageId, user.id);
+    const res = await performButtonAction(messageButton, messageId, playerId);
     if (!res) throw new InternalServerError("Button action failed");
     return res;
   }
