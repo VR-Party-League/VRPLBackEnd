dc293aacec7f937100fdf30ccb1752ea57d4cd58
Fish <conner.leuk@gmail.com>
2022-05-06 16:25:48 +0200

    Update discord, and node version

--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -1,5 +1,8 @@
 import { Client } from "discord.js";
-const client = new Client();
+
+const client = new Client({
+  intents: ["GUILDS", "GUILD_MEMBERS"],
+});
 console.log("Created discord client");
 client.login(process.env.TOKEN);
 export default client;
