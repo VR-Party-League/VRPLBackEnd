df6bb00a5ed0c04cde28f1f169aba4103460edf1
Fish <conner.leuk@gmail.com>
2022-06-17 19:19:43 +0200

    Fix for possible apollo server dos

--- src/utils/servers/createApolloServer.ts
+++ src/utils/servers/createApolloServer.ts
@@ -43,6 +43,7 @@ export async function createApolloServer() {
   return new ApolloServer({
     schema: schema,
     introspection: true,
+    persistedQueries: false,
     plugins: [
       ApolloServerPluginLandingPageGraphQLPlayground(),
       // process.env.NODE_ENV === "production"
