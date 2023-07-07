238b4fec4f49d3725d26fd3626462b4820b0d150
Fish <conner.leuk@gmail.com>
2023-07-07 17:00:35 +0200

    Updated discord.js

--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -1,7 +1,7 @@
 import { Client } from "discord.js";
 
 const client = new Client({
-  intents: ["GUILDS", "GUILD_MEMBERS"],
+  intents: ["Guilds", "GuildMembers"],
 });
 console.log("Created discord client");
 client.login(process.env.TOKEN);
